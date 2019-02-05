ad88a96cdfc73bda2599057234c8184bdc6cd5c7
Sebastian Johansson <sebben.97@hotmail.com>
2019-02-05 13:56:22 +0100

    added comment.

--- recursion.php
+++ recursion.php
@@ -1,7 +1,7 @@
 <?php
     function fakultet($number){
         if($number === 0){
-            return 1;
+            return 1; //base statement.
         }
         else {
             return $number * fakultet($number -1);
