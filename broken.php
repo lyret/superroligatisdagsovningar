7839889a261e431619aab3c35e67a4172bcc4a63
alcr33k <alex.alle@hotmail.com>
2019-02-05 13:20:58 +0100

    Create broken.php
    fixes #9

---
+++ broken.php
@@ -0,0 +1,19 @@
+<?php 
+$rand = rand(0,1);
+if ($rand == 0) {
+    http_response_code(404);
+    die();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Broken</title>
+</head>
+<body>
+    
+</body>
+</html>
