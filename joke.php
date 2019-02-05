3ff86d45bcae87ff67d99951a218d91b3d5df31b
Ali Ezadkhaha <43235023+hodhod212@users.noreply.github.com>
2019-02-05 13:41:42 +0100

    tagit bort extra material

--- joke.php
+++ joke.php
@@ -1,23 +1,8 @@
 <?php
-$name = "Ali";
-$value = 51;
-$expire = time()+(60*60*24);
-setcookie($name,$value,$expire);
-//setcookie($name,"null",$expire);
-setcookie($name,$value,time()-3600);
+
 include "history.php";
 
-echo $_SERVER['REMOTE_ADDR'].'<br>';
-print_r($_COOKIE);
-echo '<br>';
-echo $_COOKIE['Ali'];
-echo '<br>';
-if(isset($_COOKIE['Ali'])){
-    $test = $_COOKIE['Ali'];
-}else{
-    $test = "";
-}
-echo $test.'--------';
+
 ?>
 
 <?php
