fb46b717029d00fc6333f3268b3fb001d7cce94e
misticG <tuy-vi.huynh@medieinstitutet.se>
2019-02-05 14:28:21 +0100

    finished

--- register.php
+++ register.php
@@ -7,14 +7,36 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="session.php" method="post">
+    <form action="register.php" method="post">
         Förnamn: <input type="text" name="firstName">
         Efternamn: <input type="text" name="lastName">
+        E-mail: <input type="email" name="email">
         <br>
         <br>
         <input type="submit" value="Är du med?"> 
     </form>
 
-    
+<?php
+
+session_start();
+
+if(isset($_POST)) {
+    $_SESSION['regInfo'] = $_POST; 
+}
+
+    if($_SESSION['regInfo']) { 
+        $firstName = $_SESSION['regInfo']['firstName'];
+        $lastName = $_SESSION['regInfo']['lastName'];
+        $email = $_SESSION['regInfo']['email'];
+        ?>
+        <h4>Du finns: <?php echo $firstName ?>  <?php echo $lastName ?> <br> Email: <?php echo $email ?>.</h4> 
+        <?php 
+    } else {
+        ?>
+        <h4>Du finns ej än</h4> 
+        <?php
+    }
+?>
+
 </body>
 </html>
